bf4bcb13507fa0e5bd8899afc30a1843c517e557
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-11-14 23:01:41 -0600

    first steps and initial config

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const bodyParser = require ('body-parser');
 
+const response = require('./network/response')
+
 const router = express.Router();
 
 var app = express();
-app.use(router);
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(router);
+
+router.get('/message', function(req,res) {
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado",
+    })
+    response.sucess(req, res, 'Lista de mensajes');
+});
 
 router.get('/', function(req,res) {
     res.send('Hola desde get');
 });
-router.post('/', function(req,res) {
+
+
+
+
+router.post('/message', function(req,res) {
+    console.log(req.query);
+    if(req.query.error == "ok") {
+        response.error(req, res, 'Error inesperado', 500, 'Solo una simulacion')
+    } else {
+        response.sucess(req, res, 'Creado correctamente', 201);      
+    }
     console.log(req.body);
-    res.send('Hola desde post');
 });
 //app.use('/', function(req,res) {
 //    res.send('Hola');
 //})
 
+
+app.use('/app', express.static('public'));
 app.listen(3000);
 console.log('Estoy corriendo en el puerto 3000');
